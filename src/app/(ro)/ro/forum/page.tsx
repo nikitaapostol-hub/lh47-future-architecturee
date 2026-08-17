4b4c712b41d8d21a9f2476ee4693bfad59191175
nikitaapostol-hub <nikita.apostol@lh47arch.com>
2026-08-17 16:20:15 +0300

    Add files via upload

---
+++ src/app/(ro)/ro/forum/page.tsx
@@ -0,0 +1,9 @@
+import { Forum } from '@/lib/pages'
+import { meta } from '@/i18n/meta'
+
+export const dynamic = 'force-dynamic'
+export const metadata = meta('ro', '/forum')
+
+export default function Page() {
+  return <Forum lang="ro" />
+}
