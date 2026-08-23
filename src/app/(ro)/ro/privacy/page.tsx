657969c9867721307de8b640bfbe607928e85e7b
nikitaapostol-hub <nikita.apostol@lh47arch.com>
2026-08-23 19:19:39 +0300

    Privacy route (ro)

---
+++ src/app/(ro)/ro/privacy/page.tsx
@@ -0,0 +1,8 @@
+import PrivacyPage from '@/components/PrivacyPage'
+import { privacyMeta } from '@/i18n/meta'
+
+export const metadata = privacyMeta('ro')
+
+export default function Page() {
+  return <PrivacyPage lang="ro" />
+}
